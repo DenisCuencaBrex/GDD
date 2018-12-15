4da27e2c8feec0214b1f1562a2fd0e9dc04c65ed
Denis Cuenca Brex <deniscbrex.dev@gmail.com>
2018-12-15 18:54:08 -0300

    Cambios en el movimiento del DarkMage

--- Assets/Scripts/DarkMage.cs
+++ Assets/Scripts/DarkMage.cs
@@ -4,38 +4,78 @@ using UnityEngine;
 
 public class DarkMage : MonoBehaviour
 {
-
-    public int life = 1;
+    public MageStages stage;
+    public float initialLife = 100f;
+    public float actuaLife; 
     public float speed = 2f;
-    public float timer = 2f;
+    float percentage;
 
     public bool shooting = false;
 
     public Animator darkMage;
     public Transform target;
+    public Transform pos;
 
     public GameObject bullet;
+    public GameObject colorsBlock;
 
     public float fireRate = 0.5f;
     float nextFire = 1f;
 
-    void FixedUpdate()
+    public enum MageStages
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        if (Mathf.Floor(transform.position.z) > 2f)
-        {
-            Shoot();
-            speed = 0;
-            timer -= Time.deltaTime;
-            shooting = true;
+        phase1,
+        phase2,
+        phase3,
+    }
+
+    public void Start()
+    {
+        actuaLife = initialLife;
+    }
+
+    public void Update()
+    {
+        LifePercentage();
+        Phases();
+    }
+
+    void LifePercentage(){
+        percentage = (actuaLife * 100) / initialLife;
+
+        if(percentage <= 100 && percentage > 80){
+            stage = MageStages.phase1;
         }
-        if (timer <= 0){
-            speed = -2f;
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            shooting = false;
-            darkMage.SetBool("Attack", false);  
+        else if(percentage <= 80 && percentage > 60){
+            stage = MageStages.phase2;
         }
     }
+
+    void Phases(){
+        switch (stage)
+        {
+            case MageStages.phase1:
+                print("stage 1");
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                if (Mathf.Floor(transform.position.z) > 2f)
+                {
+                    shooting = true;
+                    Shoot();
+                    speed = 0;
+                }
+                break;
+
+            case MageStages.phase2:
+                print("stage 2");
+                shooting = false;
+                darkMage.SetBool("Attack", false);
+                speed = 2f;
+                transform.position = Vector3.MoveTowards(transform.position, pos.position, speed * Time.deltaTime);
+                break;
+        }
+    }
+
+
 
     void Shoot()
     {
